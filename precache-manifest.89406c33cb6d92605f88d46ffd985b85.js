ca675489d3441e086ed5f6f9388006da80bcc0b0
pumpkinzomb <pumpkinzomb@gmail.com>
2019-01-21 21:40:40 +0900

    Updates

---
+++ precache-manifest.89406c33cb6d92605f88d46ffd985b85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4165f0339e81d2c16a6",
+    "url": "/TodoList-redux/static/js/runtime~main.c4165f03.js"
+  },
+  {
+    "revision": "55a07c33d2649661dc20",
+    "url": "/TodoList-redux/static/js/main.55a07c33.chunk.js"
+  },
+  {
+    "revision": "9b559a04a00573f5c973",
+    "url": "/TodoList-redux/static/js/1.9b559a04.chunk.js"
+  },
+  {
+    "revision": "55a07c33d2649661dc20",
+    "url": "/TodoList-redux/static/css/main.e4389b44.chunk.css"
+  },
+  {
+    "revision": "56a129ba140de8e79058acde9a4be7c6",
+    "url": "/TodoList-redux/index.html"
+  }
+];
